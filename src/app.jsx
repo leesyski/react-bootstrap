0e713c7c164743c427184dcb5f4c86a35a9eb6b3
Mark Lees <marklees1@btinternet.com>
2019-04-15 11:19:02 +0100

    Location Data

--- src/app.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-
-// eslint-disable-next-line react/prefer-stateless-function
-class app extends React.Component {
-    render;
-}
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,8 @@
 import 'raf/polyfill';
 import React from 'react';
 import { render } from 'react-dom';
+import App from './components/app';
+import { location } from './data/forecast.json';
 
-render(<h1>Hello World!</h1>, document.getElementById('root'));
+
+render(<App location={location} />, document.getElementById('root'));
